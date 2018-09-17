3594a418ba966c2f2f1bf97ead74c134105f80b5
Rui Ban <iberryful@gmail.com>
2018-09-17 15:34:10 +0800

    first commit

---
+++ say-hi/app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+NAME = os.environ['NAME']
+
+@app.route("/")
+def hello():
+    return "Hi, %s, you got a flag: hello-world. Cheers!" % NAME
+
+if __name__ == '__main__':
+    app.run()
