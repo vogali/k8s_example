24f451f6b5a1ed62a521620bfbde38b71ca0e529
Rui Ban <iberryful@gmail.com>
2018-09-19 18:02:23 +0800

    update flag

--- say-hi/app.py
+++ say-hi/app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from redis import Redis
 
 app = Flask(__name__)
 
@@ -8,6 +9,21 @@ NAME = os.environ['NAME']
 @app.route("/")
 def hello():
     return "Hi, %s, you got a flag: hello-world. Cheers!\r\n" % NAME
+
+@app.route("/flag")
+def flag():
+    host = os.getenv('REDIS_HOST', None)
+    port = os.getenv('REDIS_PORT', None)
+    password = os.getenv('REDIS_PASSWORD', None)
+    if None in (host, port, password):
+        return "Looks like you don't have REDIS_PASSWORD, keep trying!\r\n"
+    r = Redis(host=host, port=port, password=password)
+    count = r.incr('pv', 1)
+    if count == 30:
+        flag = r.get('flag')
+        return "Bingo~ You got it: keep-secret\r\n"
+    return "Your access no is %d, try harder, the flag is hidden at 30" % count
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
